f2e33e27c959b49b78e7ff5907a1bf579bb7edb8
Wugaoliang <wugaoliang@xiaomi.com>
2020-11-03 10:11:02 +0800

    fix: modal

--- components/key-handler/index.jsx
+++ components/key-handler/index.jsx
@@ -9,61 +9,48 @@ const KeyHandler = ({ children, keyshandle = [], displayName }) => {
       window.Mousetrap.bind(code, (e) => {
         e.stopPropagation()
         keyshandle[code](e, document.activeElement.nodeName)
+        return false
       })
     })
   }, [keyshandle])
   const { onFocus, onBlur, onMouseEnter, onMouseLeave, onKeyDown } = children.props
+  const containerComponent = ['modal']
   return (
     <>
-      {React.cloneElement(children, {
-        onKeyDown: (e) => {
-          if (displayName && displayName === 'modal') {
-            console.log('添加按键')
-            window.Mousetrap.bind('esc', (e) => {
-              console.log('document.activeElement.nodeName', document.activeElement.nodeName)
-              keyshandle.esc(e, document.activeElement.nodeName)
-              window.Mousetrap.unbind('esc')
-            })
-          }
-          onKeyDown && onKeyDown(e)
-        },
-        tabIndex: 0,
-        onFocus: (e) => {
-          if (displayName && displayName === 'modal') {
-            onFocus && onFocus(e)
-            return
-          }
-          bindKeyCallback()
-          setIsFocus(true)
-          console.log('聚焦', e)
-        },
-        onBlur: (e) => {
-          if (displayName && displayName === 'modal') {
-            onBlur && onBlur(e)
-            return
-          }
-          !inSide && window.Mousetrap.reset()
-          setIsFocus(false)
-          console.log('失去焦点')
-        },
-        onMouseEnter: (e) => {
-          if (displayName && displayName === 'modal') {
-            onMouseEnter && onMouseEnter(e)
-            return
-          }
-          setInSide(true)
-          console.log('移入')
-        },
-        onMouseLeave: (e) => {
-          if (displayName && displayName === 'modal') {
-            onMouseLeave && onMouseLeave(e)
-            return
-          }
-          !isFocus && window.Mousetrap.reset()
-          setInSide(false)
-          console.log('移出')
-        }
-      })}
+      {displayName && containerComponent.includes(displayName)
+        ? React.cloneElement(children, {
+            onKeyDown: (e) => {
+              console.log(e.which, e.target)
+              console.log('document.activeElement.nodeName', document.activeElement)
+              onKeyDown && onKeyDown(e)
+            },
+            tabIndex: 0
+          })
+        : React.cloneElement(children, {
+            onFocus: (e) => {
+              bindKeyCallback()
+              setIsFocus(true)
+              console.log('聚焦', e)
+              onFocus && onFocus(e)
+            },
+            onBlur: (e) => {
+              !inSide && window.Mousetrap.reset()
+              setIsFocus(false)
+              console.log('失去焦点')
+              onBlur && onBlur(e)
+            },
+            onMouseEnter: (e) => {
+              setInSide(true)
+              console.log('移入')
+              onMouseEnter && onMouseEnter(e)
+            },
+            onMouseLeave: (e) => {
+              !isFocus && window.Mousetrap.reset()
+              setInSide(false)
+              console.log('移出')
+              onMouseLeave && onMouseLeave(e)
+            }
+          })}
     </>
   )
 }
